eec6a02e50031ba33c378fe92c358d5496a6a25b
bpjorgerojas <jrojas.pz@gmail.com>
2024-03-02 03:21:18 -0600

    Done - Fix bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./dist/*.html'],
+  content: ['./dist/*.html', './dist/**/*.html'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    screens: {
+      sm: '550px',
+      md: '800px',
+      lg: '1200px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      transitionDuration: {
+        0: '0ms',
+        2000: '2000ms',
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-12deg)' },
+          '50%': { transform: 'rotate(12deg)' },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
